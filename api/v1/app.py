b7edd752d27c9281c4b0d1f3f794942cc12fc608
brian <muriithibrian283@gmail.com>
2025-02-16 12:45:51 +0300

    Error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,8 +14,14 @@ def teardown_db(exception):
     """Close storage session"""
     storage.close()
 
-
+@app.errorhandler(404)
+def not_found(error):
+    """
+    
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_PORT", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(debug=True,host=host, port=port, threaded=True)
