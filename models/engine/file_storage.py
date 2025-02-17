1d1c2a5568d325c69660475cf5fe7b7245cc2c1f
EugineI <eugineindimuli@gmail.com>
2025-02-17 21:45:15 +0300

    (app.py)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,6 +40,12 @@ class FileStorage:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
+    def get(self, cls, id):
+        """Retrive object"""
+        if cls not in self.classes().values():
+            return None
+        return self.all(cls).get(id, None)
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
